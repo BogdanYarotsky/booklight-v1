84df1304972c212abbc7962a10601cccd46d0142
BogdanYarotsky <byarotsky@gmail.com>
2023-03-04 16:57:53 +0100

    added sample node app

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { Book } from "../shared/book";
 import { Constants } from "../shared/constants";
+import express from "express";
 
 
 const book: Book = {
@@ -14,3 +15,18 @@ const book: Book = {
 
 console.log(book);
 console.log(Constants.APP_NAME);
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get(["/", "/:name"], (req, res) => {
+    const greeting = "<h1>Hello From Node on Fly!</h1>";
+    const name = req.params["name"];
+    if (name) {
+        res.send(greeting + "</br>and hello to " + name);
+    } else {
+        res.send(greeting);
+    }
+});
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+
